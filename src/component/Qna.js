75efc284df45331dfdacff10279e9aec41ee2c66
soyeya <parksoye123@gmail.com>
2023-01-27 21:55:07 +0900

    react-hook-form 연동해서 데이터처리

--- src/component/Qna.js
+++ src/component/Qna.js
@@ -1,45 +1,82 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 
 const Qna = () => {
 
+    const { 
+        //폼안의 클릭함수
+        handleSubmit, 
+        //전송관련 함수
+        register, 
+        formState: { errors } } = useForm(); //유효성검사 _ 일단 error를 넣어둠 왜냐? 폼태그가 지금 공백이므로 -> 공백으로만 넘어가면 오류니까 (처음 폼이 비어있으므로 errors로 시작한다)
+
+    //검수함수
+    const onSubmit = values => console.log(values);
+
 
     //중복제출막기
-    const [statue, updateStatue] = useState(false);
-    const [values, setValues] = useState({
-        comNm: "",
-        email: "",
-        password: "",        
-        content: "",
+    // const [statue, updateStatue] = useState(false);
+    // const [values, setValues] = useState({
+    //     comNm: "",
+    //     email: "",
+    //     password: "",        
+    //     content: "",
         
-      })
+    //   })
     
-    const handleChange = (e) =>{
-        setValues({
-            ...values,
-            [e.target.name] : e.target.value
-        })
-        //input의 name을 변수이름과 같이 해서 식을 심플하게 처리
-    }
+    // const handleChange = (e) =>{
+    //     setValues({
+    //         ...values,
+    //         [e.target.name] : e.target.value
+    //     })
+    //     //input의 name을 변수이름과 같이 해서 식을 심플하게 처리
+    // }
    
-    const sendform = async(e) => {
-        e = e || window.event;
-        e.preventDefault(); //새로고침하지마
-        updateStatue(true); // 버튼막아
-        //데이터를 전송하거나 유효성검사
-        await new Promise( (r) => setTimeout(function(){
-          console.log(JSON.stringify(values, null, 10))  
-        }, 1000))
-        updateStatue(false); //버튼정상화ㄴ
-    }
+    // const sendform = async(e) => {
+    //     e = e || window.event;
+    //     e.preventDefault(); //새로고침하지마
+    //     updateStatue(true); // 버튼막아
+    //     //데이터를 전송하거나 유효성검사
+    //     await new Promise( (r) => setTimeout(function(){
+    //       console.log(JSON.stringify(values, null, 10))  
+    //     }, 1000))
+    //     updateStatue(false); //버튼정상화ㄴ
+    // }
 
     return (
         <div id="qna" className='py-5'>
             <h2 className='text-center py-5'>문의하기</h2>
 
-           
+            <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="email"
+        {...register("email", {
+          required: "Required",
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "invalid email address"
+          }
+        })}
+      />
+         {/* errors.email = true 일때만 errors.email.message 나간다 라는 뜻 -> if문임 
+      formstate의 설정해둔 errors의 이메일 에러 메세지 출력*/}
+      {errors.email && errors.email.message}  
+   
+     
 
-            <form onSubmit={ sendform } className='col-5 mx-auto'>
+      <input
+        {...register("username", {
+          validate: value => value !== "admin" || "Nice try!"
+        })}
+      />
+      {errors.username && errors.username.message}
+
+      <button type="submit">Submit</button>
+    </form>
+
+
+            {/* <form onSubmit={ sendform } className='col-5 mx-auto'>
                 <ul>
                     <li className='my-3'>                        
                         <input type="text" 
@@ -82,7 +119,7 @@ const Qna = () => {
                     <button type="submit" disabled={statue}>문의하기</button> 
                 </div>               
                                
-            </form>            
+            </form>             */}
             
         </div>
     );
